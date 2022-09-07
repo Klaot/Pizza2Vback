4c3647f2069afcaeb12cc7c4ad51a561d0f0c949
Vitaliy <klaot@yandex.ru>
2022-09-07 22:47:12 +0300

    исправил названия

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const db = [
     {
       "id": 2,
       "imageUrl": "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/6652fec1-04df-49d8-8744-232f1032c44b.jpg",
-      "name": "Цыпленок барбекю",
+      "title": "Цыпленок барбекю",
       "types": [0],
       "sizes": [26, 40],
       "price": 295,
@@ -38,7 +38,7 @@ const db = [
     {
       "id": 3,
       "imageUrl": "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/af553bf5-3887-4501-b88e-8f0f55229429.jpg",
-      "name": "Кисло-сладкий цыпленок",
+      "title": "Кисло-сладкий цыпленок",
       "types": [1],
       "sizes": [26, 30, 40],
       "price": 275,
