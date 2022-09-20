1c07fe68df0df1a3e56e50dd94d7dbede0ae87e4
Vitaliy <klaot@yandex.ru>
2022-09-21 00:52:22 +0300

    сделал правильную фильрацию

--- function/search.js
+++ function/search.js
@@ -1,5 +1,7 @@
 const searchPizza = (searchName, namePizza) => {
-    return namePizza.filter(item => item.title.includes(searchName));
+    return namePizza.filter(item => {
+        return item.title.toLowerCase().includes(searchName) 
+    });
  }
 
  module.exports = searchPizza;
